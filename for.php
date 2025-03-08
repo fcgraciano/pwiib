873742875d9b45e3a4373457bf46bbd89cb45407
Prof Etec <email@etec.com>
2025-03-07 22:37:13 -0300

    Aula de Laço de repetição 07-03-2025

--- for.php
+++ for.php
@@ -42,6 +42,26 @@
   </div>
 </nav>
 <h1>For</h1>
+<p>
+  For é uma estrutura de repetição. Existem 3 configurações para 
+  a estrutura de repetição funcionar:
+  <ol>
+    <li>Variável de controle</li>
+    <li>Condição de finalização</li>
+    <li>Incremento da variavel de controle</li>
+  </ol>
+  <pre>
+      for($i = 0; $i < 10; $i++){
+        echo "indice $i";
+      }
+  </pre>
+  <?php
+    for($i = 0; $i < 10; $i++){
+      echo "indice $i <br/>";
+    }
+  ?>
+</p>
+
 
 <script src="bootstrap.bundle.min.js"></script>
 </body>
--- while.php
+++ while.php
@@ -42,7 +42,53 @@
   </div>
 </nav>
 <h1>While</h1>
+<p>
+  While é uma estrutura de repetição. Existem 3 configurações para 
+  a estrutura de repetição funcionar:
+  <ol>
+    <li>Variável de controle</li>
+    <li>Condição de finalização</li>
+    <li>Incremento da variavel de controle</li>
+  </ol>
+  <pre>
+      $i = 0;
+      while($i < 10)
+      {
+        echo "indice $i";
+        $i = $i + 1;
+      }
+  </pre>
+  <?php
+    $i = 0;
+    while($i < 10)
+    {
+      echo "while com indice $i <br>";
+      $i = $i + 1;
+    }
+  ?>
+</p>
+<h2>Do While </h2>
 
+<p>
+A diferença do DO WHile para While é que no DO WHILE sempre vai haver pelo menos 
+uma execução do bloco de código.
+Você avalia a necessidade de qual estrutura de repetição é adequada.
+  <pre>
+      $i = 0;
+      do{
+        echo "while com indice $i <br>";
+        $i = $i + 1;
+      }while($i < 10)
+  </pre>
+<?php 
+
+$i = 0;
+      do{
+        echo "DO WHILE com indice $i <br>";
+        $i = $i + 1;
+      }while($i < 10)
+?>
+  </p>
 <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html>
