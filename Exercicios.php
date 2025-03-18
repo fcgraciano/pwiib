15cf6fe9d63b619602cd31c3b70319952e2fdfcb
Prof Etec <email@etec.com>
2025-03-18 20:23:10 -0300

    Array Chave valor e exercicio

---
+++ Exercicios.php
@@ -0,0 +1,24 @@
+1- Crie um formulário HTML que solicite o 
+    nome e a idade de um usuário.
+ Ao enviar o formulário com o método GET, 
+ exiba o nome e a idade do usuário na página seguinte.
+
+2-Crie um formulário HTML que receba dois números e, 
+ao enviar com o método GET, exiba a soma desses dois números.
+
+3-Crie um formulário HTML que peça o nome do usuário. 
+Quando o formulário for enviado com o método GET,
+ exiba uma mensagem personalizada dizendo "Olá, [nome]".
+
+4-Crie um formulário de login que receba o nome de usuário e senha.
+ Quando o formulário for enviado com o método POST,
+  verifique se o nome de usuário e a senha são válidos
+   (pode ser um simples exemplo de validação).
+
+5-Crie um formulário HTML que receba dois números e,
+ ao enviar com o método POST, exiba o produto desses números.
+
+6-Crie um formulário HTML que receba
+ o nome e a mensagem de um usuário.
+ Quando o formulário for enviado com o método POST,
+  exiba a mensagem do usuário.
---
+++ salvar_dados.php
@@ -0,0 +1,11 @@
+<?php 
+
+    echo $_GET["login"];
+    echo "<br/>";
+    echo $_GET["senha"];
+    echo "<br> daqui para baixo é post";
+    echo $_POST["login"];
+    echo "<br/>";
+    echo $_POST["senha"];
+
+?>
