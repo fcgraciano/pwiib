34a0f9bf7d18f37bd79509be038a530d08553a9f
Prof Etec <email@etec.com>
2025-04-11 22:41:08 -0300

    Piscando o botão com javascript

--- index.php
+++ index.php
@@ -2,4 +2,23 @@
 
 <?php echo "<h1> Index do nosso projeto</h1>"; ?>
 
+<button class="btn btn-success" onclick="Piscar(this)" >
+    Piscando meu botão
+</button>
+
+<script>
+    function Piscar(botao)
+    {
+        if(botao.classList.contains('btn-success'))
+        {
+            botao.classList.remove('btn-success')
+            botao.classList.add('btn-danger')
+        }else{
+            botao.classList.remove('btn-danger')
+            botao.classList.add('btn-success')
+        }
+    }
+</script>
+
+
 <?php include "rodape.php"; ?>
